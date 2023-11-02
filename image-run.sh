1c77365e7da98b5aa74ae63774b0e6c53f242162
cvicens <carlos.vicens.alonso@gmail.com>
2023-11-02 17:37:46 +0100

    permissions/MODEL_PATH fixed

--- image-run.sh
+++ image-run.sh
@@ -4,9 +4,8 @@
 
 # --entrypoint bash \
 
-podman run -it --rm -p 5000:5000 \
-  -e MODEL_PATH=/deployment/model.joblib \
-  --volume kubeconfig:/var/kubeconfig \
+podman run -it --rm -p 6000:5000 \
+  -e MODEL_PATH=/models/model.joblib \
+  --volume $(pwd)/models:/models:z \
   --user 1234 \
   --name bk localhost/${PROJECT_ID}:${VERSION}
-
