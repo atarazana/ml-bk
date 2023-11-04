a1230ce610a7a88158d837d332c9f20f4859617e
cvicens <carlos.vicens.alonso@gmail.com>
2023-11-04 12:08:39 +0100

    version ok

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-DEPLOYMENT_NS=kitchensink-bot
-ENV_FILE="../.env.deploy"
+DEPLOYMENT_NS="4-test"
+# ENV_FILE="../.env.deploy"
 
-if ! test -f "${ENV_FILE}"; then
-    echo "${ENV_FILE} does not exist."
-    exit 1
-fi
+# if ! test -f "${ENV_FILE}"; then
+#     echo "${ENV_FILE} does not exist."
+#     exit 1
+# fi
 
-. ${ENV_FILE}
+# . ${ENV_FILE}
 
 oc new-project ${DEPLOYMENT_NS}
 
-oc delete secret kitchensink-bot-data -n ${DEPLOYMENT_NS}
-oc create secret generic kitchensink-bot-data --from-file=../data -n ${DEPLOYMENT_NS}
+oc delete secret ml-models -n ${DEPLOYMENT_NS}
+oc create secret generic ml-models --from-file=../models/model.joblib -n ${DEPLOYMENT_NS}
 
-oc delete secret kitchensink-bot-env -n ${DEPLOYMENT_NS}
-oc create secret generic kitchensink-bot-env --from-env-file=${ENV_FILE} -n ${DEPLOYMENT_NS}
+# oc delete secret ml-models -n ${DEPLOYMENT_NS}
+# oc create secret generic ml-models --from-env-file=${ENV_FILE} -n ${DEPLOYMENT_NS}
 
-oc apply -n ${DEPLOYMENT_NS} -f deploy.yaml
+helm template ml ml/ --set organization=${DEPLOYMENT_NS} | oc apply -n ${DEPLOYMENT_NS} -f -
 
-oc start-build bc/kitchensink-bot -n ${DEPLOYMENT_NS}
+# oc start-build bc/kitchensink-bot -n ${DEPLOYMENT_NS}
